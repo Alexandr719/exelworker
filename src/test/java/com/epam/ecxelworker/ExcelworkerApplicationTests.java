f7a2350ba4faf4eacdffa42926068342a2a0f4db
Alexander Filatov <Alexander_Filatov@epam.com>
2019-04-10 16:35:15 +0300

    messatge

---
+++ src/test/java/com/epam/ecxelworker/ExcelworkerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.epam.ecxelworker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExcelworkerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
